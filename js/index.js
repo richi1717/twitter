7fb9548c5bea611659f25b940dac19e7a0b1d45c
Richi Jeffery <kikki_sawn@yahoo.com>
2015-07-15 18:11:50 -0700

    fix screwup

--- js/index.js
+++ js/index.js
@@ -10,38 +10,63 @@ var currentUser = {
 };
 
 $(function () {
-	var usersUrl = 'http://localhost:3000/users/'
-	var tweetsUrl = 'http://localhost:3000/tweets/'
-	var repliesUrl = 'http://localhost:3000/replies/'
+  var usersUrl = 'http://localhost:3000/users/'
+  var tweetsUrl = 'http://localhost:3000/tweets/'
+  var repliesUrl = 'http://localhost:3000/replies/'
 
-	function getUsers() {
+  function getUsers() {
     return $.get(usersUrl)
-    	// .done(function (getUsersFromUserUrl) {
-    	// 	console.log(getUsersFromUserUrl)
-    	// })
+      // .done(function (getUsersFromUserUrl) {
+      //  console.log(getUsersFromUserUrl)
+      // })
   }
 
   getUsers()
-  	.done(function (users) {
-  		users.forEach(function (user) {
-  			getTweets(user.id)
-  				.done(function (tweets) {
-  					tweets.forEach(function (tweet) {
-  						console.log(tmpl.tweet({tweetid: tweet.id}))
-  						
-  					})
-  					console.log('tweets per user', tweets)
-  				})
-  		})
-  	})
+    .done(function (users) {
+      users.forEach(function (user) {
+        console.log(tmpl.tweet({
+          handle: user.handle, 
+          img: user.img,
+          message: user.realName
+        }))
+        getTweets(user.id)
+          .done(function (tweets) {
+            tweets.forEach(function (tweet) {
+              console.log(tmpl.tweet({
+                id: tweet.id,
+                userId: tweet.userId,
+                message: tweet.message
+              }))
+            })
+            
+            console.log('tweets per user', tweets)
+          getReplies()
+
+          })
+
+
+
+        })
+    })
+
+  function getReplies(id) {
+    return $.get(tweetsUrl + id + '/replies')
+      // .done(function (getRepliesFromTweetsUrl) {
+      // console.log('chris p', tweetsUrl + id + '/replies')
+        
+      // })
+  }
 
   function getTweets(id) {
-  	return $.get(usersUrl + id + '/tweets')	
-  		// .done(function (getTweetsFromTweetsUrl) {
-  		// 	console.log(getTweetsFromTweetsUrl)
-  		// })
+    return $.get(usersUrl + id + '/tweets') 
+      // .done(function (getTweetsFromTweetsUrl) {
+      //  console.log(getTweetsFromTweetsUrl)
+      // })
   }
+
+ getReplies(2)
+
+
 
   
 });
-
